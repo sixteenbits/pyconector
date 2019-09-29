8247bfb49335c141d8668c690472f90fe39f9256
crisconru <crisconru@gmail.com>
2019-09-29 09:37:11 +0200

    rulando

--- conect.py
+++ conect.py
@@ -1,54 +1,37 @@
 import paho.mqtt.client as mqtt
-from pyKey import pressKey, releaseKey, press, sendSequence, showKeys
+from pyKey import press
 
 BROKER_URL = '172.26.27.102'
 BROKER_PORT = 1883
 TOPIC = 'jamtoday/vara'
 CMD = {
-    'l': izquierda,
-    'r': derecha,
-    'v': varazo
+    'l': 'a',
+    'r': 'd',
+    'v': 's'
 }
 
 
-def izquierda():
-    print('Izquierda')
-
-
-def derecha():
-    print('derecha')
-
-
-def varazo():
-    print('varazo')
-
-
-# The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
+# Funcion que se ejecuta al conectarse al broker MQTT
+def conectado_al_broker(client, userdata, flags, rc):
+    print('Conectado con resultado {}'.format(str(rc)))
+    # Suscribirse al topic
     client.subscribe(TOPIC)
 
 
-# The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
-    CMD[msg]()
-    '''
-    if(msg == 'l'):
-        print(msg)
-    elif(msg):
-        print(msg)
-    else:
-        print(msg)
-    '''
+# Funcion que se ejecuta cuando se recibe un mensaje nuevo
+def leer_mensaje_nuevo(client, userdata, msg):
+    mensaje = msg.payload.decode('UTF-8')
+    # print(msg.topic + " " + str(msg.payload))
+    print(msg.topic + " " + mensaje)
+    comando = CMD.get(mensaje, 'nanai de la china')
+    print('tecla -> {}'.format(comando))
+    # Pulsacion de teclado
+    press(comando)
 
 
 client = mqtt.Client()  # crea el cliente
-client.on_connect = on_connect  # setup cuando se inicia la conexion
-client.on_message = on_message  # callback para cuando lee el topic
+client.on_connect = conectado_al_broker  # setup cuando se inicia la conexion
+client.on_message = leer_mensaje_nuevo  # callback para cuando lee el topic
 
 client.connect(BROKER_URL, BROKER_PORT, 60)
 # Blocking call that processes network traffic, dispatches callbacks and
